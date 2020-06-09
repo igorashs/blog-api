e596b11b5743f01ceed02216741cdffa4386eac0
igorashs <igorashsirbu@gmail.com>
2020-06-09 21:14:13 +0300

    add post

--- models/comment.js
+++ models/comment.js
@@ -6,7 +6,8 @@ const Schema = mongoose.Schema;
 const commentSchema = new Schema({
   username: { type: String, required: true, max: 15 },
   date: { type: Date, default: new Date() },
-  text: { type: String, required: true, max: 300 }
+  text: { type: String, required: true, max: 300 },
+  post: { type: mongoose.Types.ObjectId, required: true }
 });
 
 commentSchema.virtual('timestamp').get(function () {
